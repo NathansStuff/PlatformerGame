0fecaa6c4d6906daf9fd1f54b2e6c397d13b1af9
Nathanstuff <n.odonnell@hotmail.com>
2020-12-27 02:36:39 +1100

    refactored map code

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,12 +10,23 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        const map = this.make.tilemap({key: 'map'});
-        const tileset1 = map.addTilesetImage('main_lev_build_1', 'tiles-1');
-        const tileset2 = map.addTilesetImage('main_lev_build_2', 'tiles-2');
+        const map = this.createMap();
+        const layers = this.createLayers(map);
+    }
 
-        map.createStaticLayer('environment', [tileset1, tileset2]);
-        map.createStaticLayer('platforms', [tileset1,tileset2]);
+    createMap() {
+        const map = this.make.tilemap({key: 'map'});
+        map.addTilesetImage('main_lev_build_1', 'tiles-1');
+        map.addTilesetImage('main_lev_build_2', 'tiles-2');
+        return map;
+    }
+
+    createLayers(map) {
+        const tileset1 = map.getTileset('main_lev_build_1');
+        const tileset2 = map.getTileset('main_lev_build_2');
+        const environment = map.createStaticLayer('environment', [tileset1, tileset2]);
+        const platforms = map.createStaticLayer('platforms', [tileset1,tileset2]);
+        return { environment, platforms };
     }
 }
 
