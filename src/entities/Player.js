197a6e1b5883559303bb3ec3422a7bc5327ef5b0
Nathanstuff <n.odonnell@hotmail.com>
2021-01-01 02:50:05 +1100

    melee weapon

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -4,6 +4,7 @@ import collidable from '../mixins/collidable';
 import HealthBar from '../hud/HealthBar';
 import anims from '../mixins/anims';
 import Projectiles from '../attacks/Projectiles';
+import MeleeWeapon from '../attacks/MeleeWeapon';
 
 class Player extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y) {
@@ -37,6 +38,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         this.setCollideWorldBounds(true);
         this.setOrigin(0.5, 1)
         this.projectiles = new Projectiles(this.scene);
+        this.meleeWeapon = new MeleeWeapon(this.scene, 0, 0, 'sword-attack');
 
         this.health = 100;
         this.hp = new HealthBar(
@@ -51,6 +53,11 @@ class Player extends Phaser.Physics.Arcade.Sprite {
                 this.projectiles.fireProjectile(this);
                 
             
+            })
+
+            this.scene.input.keyboard.on('keydown-E', () => {
+                this.play('throw', true);
+                this.meleeWeapon.swing(this);
             })
     }
 
