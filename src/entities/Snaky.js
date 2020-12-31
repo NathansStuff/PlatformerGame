1c9e3bcadf853099776ceb08cec0194547ea6f16
Nathanstuff <n.odonnell@hotmail.com>
2021-01-01 03:45:35 +1100

    snaky shoots fireballs randomly

--- src/entities/Snaky.js
+++ src/entities/Snaky.js
@@ -1,10 +1,17 @@
 import Enemy from './Enemy';
 import initAnims from './anims/snakyAnims';
+import Projectiles from '../attacks/Projectiles';
+
 
 class Snaky extends Enemy {
     constructor(scene, x, y) {
         super(scene, x, y, 'snaky');
         initAnims(scene.anims);
+
+        this.projectiles = new Projectiles(this.scene, 'fireball-1');
+        this.timeFromLastAttack = 0;
+        this.attackDelay = this.getAttackDelay();
+    
     }
 
     init() {
@@ -17,12 +24,23 @@ class Snaky extends Enemy {
     update(time, delta) {
         super.update(time, delta);
 
+        if (this.timeFromLastAttack + this.attackDelay <= time) {
+            this.projectiles.fireProjectile(this);
+      
+            this.timeFromLastAttack = time;
+            this.attackDelay = this.getAttackDelay();
+          }
+
         if (!this.active) {return;}
 
         if (this.isPlayingAnims('snaky-hurt')) {return;}
 
         this.play('snaky-walk', true);
     }
+
+    getAttackDelay() {
+        return Phaser.Math.Between(1000, 4000);
+      }
 
     takesHit(source) {
         super.takesHit(source);
