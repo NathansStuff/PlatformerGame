46b1cbdfcab6fda6f636fb473a9887260fbe0241
Nathanstuff <n.odonnell@hotmail.com>
2021-01-01 03:12:06 +1100

    melee cooldown

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -5,6 +5,7 @@ import HealthBar from '../hud/HealthBar';
 import anims from '../mixins/anims';
 import Projectiles from '../attacks/Projectiles';
 import MeleeWeapon from '../attacks/MeleeWeapon';
+import { getTimestamp } from '../utils/functions';
 
 class Player extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y) {
@@ -39,6 +40,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         this.setOrigin(0.5, 1)
         this.projectiles = new Projectiles(this.scene);
         this.meleeWeapon = new MeleeWeapon(this.scene, 0, 0, 'sword-default');
+        this.timeFromLastSwing = null;
 
         this.health = 100;
         this.hp = new HealthBar(
@@ -56,8 +58,15 @@ class Player extends Phaser.Physics.Arcade.Sprite {
             })
 
             this.scene.input.keyboard.on('keydown-E', () => {
+
+                if (this.timeFromLastSwing &&
+                    this.timeFromLastSwing + this.meleeWeapon.attackSpeed > getTimestamp()) {
+                      return;
+                }
+
                 this.play('throw', true);
                 this.meleeWeapon.swing(this);
+                this.timeFromLastSwing = getTimestamp();
             })
     }
 
