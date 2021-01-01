24624935e40de73e0bebf12e464a169cf3100034
Nathanstuff <n.odonnell@hotmail.com>
2021-01-01 18:55:17 +1100

    collectable layer

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -153,11 +153,7 @@ class Play extends Phaser.Scene {
 
     createCollectables(collectableLayer) {
         const collectables = new Collectables(this).setDepth(-1);
-    
-        collectableLayer.objects.forEach(collectableO => {
-          collectables.get(collectableO.x, collectableO.y, 'diamond')
-        })
-    
+        collectables.addFromLayer(collectableLayer);
         collectables.playAnimation('diamond-shine');
     
         return collectables;
