ce99d1629483f8d37560c841ed179417c7f33dfa
Nathanstuff <n.odonnell@hotmail.com>
2021-01-01 18:07:16 +1100

    collectible anim

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -17,6 +17,9 @@ class Play extends Phaser.Scene {
 
     create() {
         const map = this.createMap();
+        initAnims(this.anims);
+
+        
         const layers = this.createLayers(map);
         const playerZones = this.getPlayerZones(layers.playerZones);
         const player = this.createPlayer(playerZones.start);
@@ -41,7 +44,6 @@ class Play extends Phaser.Scene {
         this.createEndOfLevel(playerZones.end, player);
         this.setupFollowupCameraOn(player);
 
-        initAnims(this.anims);
     }
 
     drawDebug(layer) {
@@ -155,6 +157,9 @@ class Play extends Phaser.Scene {
           collectables.get(collectableO.x, collectableO.y, 'diamond').setDepth(-1);
         })
     
+        collectables.playAnimation('diamond-shine');
+        debugger
+
         return collectables;
     
     }
